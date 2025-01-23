bfcbe81a2960ac9342364139037660c1757fad1a
rarlala <wtw9611@gmail.com>
2025-01-23 22:51:26 +0900

    init: setting default folders and files

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OrderDetails = ({ params }) => {
+  const { id } = params;
+
+  return <div>OrderDetails</div>;
+};
+
+export default OrderDetails;
---
+++ src/components/autoSignInCheckbox/AutoSignInCheckbox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AutoSignInCheckbox = () => {
+  return <div>AutoSignInCheckbox</div>;
+};
+
+export default AutoSignInCheckbot;
+x;
