79db01d76c8fafaa8b0e029ddbd7e8e6ca831a8e
rarlala <wtw9611@gmail.com>
2025-01-29 01:07:24 +0900

    feat: add toast provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import ToastProvider from "@/components/toastProvider/ToastProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ToastProvider />
         {children}
       </body>
     </html>
--- src/components/toastProvider/ToastProvider.jsx
+++ src/components/toastProvider/ToastProvider.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ToastProvider = () => {
-  return <div>ToastProvider</div>;
+  return <ToastContainer autoClose={2000} />;
 };
 
 export default ToastProvider;
